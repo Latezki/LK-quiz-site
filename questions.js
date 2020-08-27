a16a67314299ff208a9c0928a5e3bc5f463748ab
Latezki <kovanen.lauri@gmail.com>
2020-08-27 19:26:22 +0300

    Fixed window sizing issues and made questions into a new file

---
+++ questions.js
@@ -0,0 +1,98 @@
+// Questions for the quiz
+const questions = [
+    {
+        question: "Which of the animals is the biggest?",
+        answers: ["Mouse", "Horse", "Elephant", "Lion"],
+        correct: "c"
+    },
+    {
+        question: "What is the capital city of Finland?",
+        answers: ["Helsinki", "Paris", "Tokyo", "Stockholm"],
+        correct: "a"
+    },
+    {
+        question: "What does Au stand for in the periodic table?",
+        answers: ["Silver", "Gold", "Sodium", "Argon"],
+        correct: "b"
+    },
+    {
+        question: "What is the capital city of Iceland?",
+        answers: ["Prague", "Reykjavik", "Vilnius", "Berlin"],
+        correct: "b"
+    },
+    {
+        question: "Who played James Bond in the film Casino Royale(2006)?",
+        answers: ["Sean Connery", "Roger Moore", "Daniel Craig", "Timothy Dalton"],
+        correct: "c"
+    },
+    {
+        question: "Who was the original author of Dracula?",
+        answers: ["Bram Stoker", "Robert Louis Stevenson", "Daphne du Maurier", "Stephen King"],
+        correct: "a"
+    },
+    {
+        question: "Which painter painted The Scream?",
+        answers: ["Vincent van Gogh", "Leonardo Da Vinci", "Paul Gauguin", "Edvard Munch"],
+        correct: "d"
+    },
+    {
+        question: "Which group performed the song Run to the Hills?",
+        answers: ["Metallica", "Anthrax", "Iron Maiden", "AC/DC"],
+        correct: "c"
+    },
+    {
+        question: "Which group performed the song A Hard Day's Night?",
+        answers: ["The Beatles", "Queen", "Beach Boys", "Eagles"],
+        correct: "a"
+    },
+    {
+        question: "Where could you see Diocletian's Palace?",
+        answers: ["Denmark", "Italy", "Croatia", "Austria"],
+        correct: "c"
+    },
+    {
+        question: "What is the biggest state in USA?",
+        answers: ["Alaska", "Texas", "California", "Florida"],
+        correct: "a"
+    },
+    {
+        question: "Where could you see Christ the Redeemer?",
+        answers: ["Finland", "Mexico", "Canada", "Brazil"],
+        correct: "d"
+    },
+    {
+        question: "Which of the following artists has the most Grammy wins?",
+        answers: ["Beyonce", "Jay-z", "John Williams", "Bruce Springsteen"],
+        correct: "a"
+    },
+    {
+        question: "Which of the following artists has the most Grammy nominations?",
+        answers: ["Eminem", "Alicia Keys", "Stevie Wonder", "Kanye West"],
+        correct: "c"
+    },
+    {
+        question: "How fast is the speed of sound in air?",
+        answers: ["735 km/h", "1235 km/h", "2100 km/h", "1870 km/h"],
+        correct: "b"
+    },
+    {
+        question: "Where could you see the Spanish Steps?",
+        answers: ["Spain", "Greece", "Italy", "Germany"],
+        correct: "c"
+    },
+    {
+        question: "What is the population of China (2018)?",
+        answers: ["1,4 billion", "900 million", "2,1 billion", "1,9 billion"],
+        correct: "a"
+    },
+    {
+        question: "Which of the birds is the biggest?",
+        answers: ["Penguin", "Ostrich", "Bald Eagle", "Emu"],
+        correct: "d"
+    },
+    {
+        question: "What is the freezing point of ethanol?",
+        answers: ["-240 °C", "-49 °C", "-114 °C", "-424 °C"],
+        correct: "c"
+    }
+]
